185e6e6c8e52e2b4e378e489b464863f759c3b59
Yannick Goumaz <yannick.goumaz@nagra.com>
2025-03-06 11:09:34 +0100

    ping db stale connections

--- backend/config.py
+++ backend/config.py
@@ -3,6 +3,12 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///barber.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ENGINE_OPTIONS = {
+        "pool_pre_ping": True,
+        "pool_recycle": 1800,  # Recycle connections after 30 minutes
+        "pool_size": 10,        # Number of connections in the pool
+        "max_overflow": 5       # Extra connections beyond pool_size
+    }
     DEBUG = os.getenv('FLASK_DEBUG', 'False') == 'True'  # Default to False in production
 
 config = Config()
